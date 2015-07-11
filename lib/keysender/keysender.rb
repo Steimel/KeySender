c78eddd5978b9f7c6e9eaccb280ca5bf34e36bb0
Tommy Steimel <trs3ww@virginia.edu>
2015-07-11 12:44:25 -0400

    Add keysending for linux and stub for windows

--- lib/keysender/keysender.rb
+++ lib/keysender/keysender.rb
@@ -5,12 +5,24 @@ module KeySender
 
   def self.send_to_os(text, os)
     case os
+      when :windows
+        send_windows(text)
       when :mac
         send_mac(text)
+      when :linux
+        send_linux(text)
     end
+  end
+
+  def self.send_windows(text)
+
   end
 
   def self.send_mac(text)
     system('osascript', '-e', "tell application \"System Events\" to keystroke \"#{text}\"")
+  end
+
+  def self.send_linux(text)
+    system('xdotool', 'type', text)
   end
 end
